a2a1233d059b2d8dc970deacbd8693e683232e64
SevenCatsHere <snowcolton@hotmail.com>
2020-03-29 16:07:59 -0400

    add tripbot source

---
+++ packages/tripbot/src/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const Irc = require('irc-framework');
+
+const client = new Irc.Client();
+client.connect({
+  host: process.env.IRC_HOST,
+  port: process.env.IRC_PORT,
+  nick: process.env.IRC_NICK,
+});
+
+client.on('debug', console.error);
+
+client.on('registered', () => {
+  client.say('NickServ', `identify ${process.env.IRC_PASSWORD}`);
+});
+
+client.on('message', (event) => {
+  console.log(event.message);
+});

a49a188fd1329edb819e8d86f11f76c294ab04b6
SevenCatsHere <snowcolton@hotmail.com>
2020-03-30 01:27:56 -0400

    change psql port

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,24 +2,27 @@
 
 if [[ -d "node_modules" ]]; then
     echo "Removing generated files..."
-    rm -rf node_modules
-    npm i
+    rm -rf node_modules packages/*/node_modules
     echo ""
 fi
 
 container_ids=$(docker ps -qf name=tripsit)
 if [[ ! -z "$container_ids" ]]; then
-    echo "\nRemoving docker containers and volumes..."
+    echo "Removing docker containers and volumes..."
     docker stop $container_ids
     docker rm $container_ids
     docker volume rm $(docker volume ls -qf name=tripsit)
     echo ""
 fi
 
-echo "\nRunning clean tasks in packages..."
+echo "Install base dependencies..."
+npm i
+echo ""
+
+echo "Running clean tasks in packages..."
 npx lerna run clean
 echo ""
 
-echo "\nBootstrapping packages..."
+echo "Bootstrapping packages..."
 npx lerna bootstrap
 echo ""
